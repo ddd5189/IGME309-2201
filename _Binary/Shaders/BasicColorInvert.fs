2d1f9e5fb8a40077c7fc9032f6c1b9ff0248fda2
Drew Donovan <ddd5189@rit.edu>
2020-09-03 15:56:38 -0400

    Basic Color Invert Fragment Shader
    Works so that it inverts the color if the bool is true, and doesn't if it is false.

--- _Binary/Shaders/BasicColorInvert.fs
+++ _Binary/Shaders/BasicColorInvert.fs
@@ -2,25 +2,42 @@
 
 in vec3 Color;
 
+uniform bool Complimentary;
+
 uniform vec3 SolidColor = vec3(-1,-1,-1);
-uniform bool Complimentary = false;
 
 out vec4 Fragment;
 
 void main()
 {
-	vec3 output = Color;
-	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
-	{
-		output = SolidColor;
-	}
-	
-	if(Complimentary == true)
-	{
-		output = vec3(1.0f, 1.0f, 1.0f) - output;
-	}
-	
-	Fragment = vec4(output,1);
+	vec3 newColor;
 		
+	if(Complimentary) {
+	
+			newColor.r = 1 - Color.r;
+			newColor.g = 1 - Color.g;
+			newColor.b = 1 - Color.b;
+
+		
+		Fragment = vec4(newColor, 1);
+	}
+	else 
+		Fragment = vec4(Color,1);
+		
+
+	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0){
+		if(Complimentary) {
+		
+			
+			newColor.r = 1 - SolidColor.r;
+			newColor.g = 1 - SolidColor.g;
+			newColor.b = 1 - SolidColor.b;
+			
+			Fragment = vec4(newColor, 1);
+		}
+		else
+			Fragment = vec4(SolidColor, 1);
+	}
+
 	return;
 }
