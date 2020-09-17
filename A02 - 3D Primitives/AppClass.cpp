90021b1d16202f24c90475a189684cd42c4496bb
Drew Donovan <ddd5189@rit.edu>
2020-09-16 21:33:52 -0400

    A02 Fixing changed values
    A02 Fixing changed values for subdivisions when creating the sphere to make it look better

--- A02 - 3D Primitives/AppClass.cpp
+++ A02 - 3D Primitives/AppClass.cpp
@@ -18,8 +18,8 @@ void Application::InitVariables(void)
 	m_pTube = new MyMesh();
 	m_pTube->GenerateTube(0.5f, 0.35f, 1.0f, 7, C_GREEN);
 	m_pSphere = new MyMesh();
-	// I changed this to 10 to make it look better
-	m_pSphere->GenerateSphere(0.5f, 10, C_BLUE);
+	// This looks a lot beter with 10
+	m_pSphere->GenerateSphere(0.5f, 3, C_BLUE);
 	m_pTorus = new MyMesh();
 	m_pTorus->GenerateTorus(0.5f, 0.30f, 7, 7, C_VIOLET);
 
